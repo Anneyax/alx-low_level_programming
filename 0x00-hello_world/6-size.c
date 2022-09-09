40546f1502294681b22e6c626a481ede142e48d5
Anneyax <anneyakobeth@gmail.com>
2022-09-09 13:53:24 -0700

    My C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- * main - prints thre size of various types
- * Return: 0 if exited ptoperly, non-zero otherwise
+ * main - prints size of characters
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n",sizeof(int));
-	printf("size of a long int: %d byte(s)\n",sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n",sizeof(long long int));
-	printf("size of a float: %d byte(s)\n",sizeof(float));
-	return (0);
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d bytes(s)\n", sizeof(int));
+printf("size of long int: %d byte(s)\n", sizeof(long int));
+printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of float: %d byte(s)\n", sizeof(float));
+return (0);
 }
