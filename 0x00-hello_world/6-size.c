f9ca82da8090a1ee9ec090beff38965a6d5fc208
Anneyax <anneyakobeth@gmail.com>
2022-09-09 15:05:08 -0700

    My C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-	long longType;
-	/* sizeof evaluates the size of a avariable*/
-	printf("size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("size of an int: %zu byte(s)\n", sizeof(intType));
-	printf("size of a long int: %zu byte(s)\n", sizeof(longType));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(doubleType));
-	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
-	return (0);
+int intType;
+float floatType;
+double doubleType;
+char charType;
+long longType;
+/* sizeof evaluates the size of a avariable*/
+printf("size of a char: %zu byte(s)\n", sizeof(charType));
+printf("size of an int: %zu byte(s)\n", sizeof(intType));
+printf("size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
 }
 
