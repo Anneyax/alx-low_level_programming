b89a1acad7d8a9a744b2b10550efd4d3c07ed06a
Anneyax <anneyakobeth@gmail.com>
2022-09-09 14:04:30 -0700

    My C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - prints size of characters
- *
- * Return: Always 0 (success)
+ * main - prints size of various types
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
